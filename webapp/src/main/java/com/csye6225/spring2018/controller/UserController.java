b0c8692bd567b4c8c7889d3e4794c0fc32e881fe
Akshay <gurao.a@husky.neu.edu>
2018-01-25 18:42:53 -0500

    Added DAO

--- webapp/src/main/java/com/csye6225/spring2018/controller/UserController.java
+++ webapp/src/main/java/com/csye6225/spring2018/controller/UserController.java
@@ -1,14 +1,25 @@
 package com.csye6225.spring2018.controller;
 
+import com.csye6225.spring2018.DAO.UserDAO;
+import com.csye6225.spring2018.pojo.User;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import javax.validation.Valid;
+
 
 @Controller
 public class UserController {
     private final static Logger logger = LoggerFactory.getLogger(UserController.class);
+
+    @Autowired
+    UserDAO userDao;
 
     @RequestMapping("/register")
     public String index() {
@@ -22,9 +33,19 @@ public class UserController {
         return "UserHome";
     }
 
-    @RequestMapping(value = "/registerUser" , method = RequestMethod.POST)
-    public String index2() {
+    @PostMapping("/registerUser")
+    public String createUser(@Valid @RequestParam String email, @RequestParam String password) {
         logger.info("Loading index page after successful registration.");
+        User user = new User();
+        user.setEmail(email);
+        user.setPassword(password);
+        try {
+            userDao.register(user);
+            logger.info("User added to database successfully");
+        } catch (Exception e) {
+            logger.info("Error adding user to database");
+            e.printStackTrace();
+        }
         return "index";
     }
 }
---
+++ webapp/src/main/java/com/csye6225/spring2018/pojo/User.java
@@ -0,0 +1,43 @@
+package com.csye6225.spring2018.pojo;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "user_table")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    @Column(name = "personID", unique=true, nullable = false)
+    private int id;
+
+    @Column(name="Email")
+    private String email;
+
+    @Column(name = "Password")
+    private String password;
+
+    public User(){
+
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
