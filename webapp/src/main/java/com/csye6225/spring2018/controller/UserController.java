b744cb92f7c4f18270066dc27d69e163cba0cde3
Akshay <gurao.a@husky.neu.edu>
2018-01-31 15:46:03 -0500

    Added rest services

--- webapp/src/main/java/com/csye6225/spring2018/controller/UserController.java
+++ webapp/src/main/java/com/csye6225/spring2018/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.csye6225.spring2018.controller;
 
+import com.csye6225.spring2018.GenericResponse;
 import com.csye6225.spring2018.UserRepository;
 import com.csye6225.spring2018.User;
 
@@ -7,31 +8,22 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCrypt;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
-import java.util.Date;
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.Valid;
 
 
-@Controller
+@RestController
 public class UserController {
     private final static Logger logger = LoggerFactory.getLogger(UserController.class);
 
     @Autowired
     private UserRepository userRepository;
 
-    @RequestMapping("/register")
-    public String viewRegisterPage() {
-        logger.info("Loading registration page.");
-        return "register";
-    }
 
-    @RequestMapping("/loginUser")
     @PostMapping("/loginUser")
     public String login(@RequestParam String email, @RequestParam String password) {
-
         for (User u : userRepository.findAll()){
         if (u.getEmail().equals(email) && BCrypt.checkpw(password,u.getPassword())){
         logger.info("Loading user home page.");
@@ -43,14 +35,18 @@ public class UserController {
         return "error";
     }
 
-    @PostMapping("/registerUser")
-    public String createUser(@RequestParam String email, @RequestParam String password) {
-        logger.info("Loading index page after successful registration.");
+    @RequestMapping(value = "/registerUser", method = RequestMethod.POST)
+    @ResponseBody
+    public GenericResponse createUser(@Valid User user, HttpServletRequest request){
+        logger.info("Registering user account with information: {}", user);
         User newUser = new User();
-        newUser.setEmail(email);
-        newUser.setPassword(BCrypt.hashpw(password,BCrypt.gensalt()));
+        newUser.setEmail(String.valueOf(user.getEmail()));
+        newUser.setPassword(BCrypt.hashpw(String.valueOf(user.getPassword()),BCrypt.gensalt()));
         userRepository.save(newUser);
-        return "index";
+        String appUrl = "http://" + request.getServerName() + ":" +
+                request.getServerPort() + request.getContextPath();
+
+        return new GenericResponse("success");
     }
 
     @RequestMapping("/login")
