119de274d98169d6522ccc31440a49a9f08d6825
s-rohil <subramanian.roh@northeastern.edu>
2018-02-03 01:48:10 -0500

    added validation to scripts

--- infrastructure/aws/cloudformation/csye6225-aws-cf-create-stack.sh
+++ infrastructure/aws/cloudformation/csye6225-aws-cf-create-stack.sh
@@ -1,21 +1,22 @@
 #!/bin/bash
 var1="$1"
 
-# name=$(aws cloudformation describe-stacks --stack-name rohiltest --query "Stacks[*].StackName" --output text)
-# echo $?
-# echo $name
+#name=$(aws cloudformation describe-stacks --stack-name $var1 --query "Stacks[*].StackName" --output text 2>&1)
+#echo $?
 
-# if [ "$?" -eq "255" ];then		
-# 	continue
-# else
-# 	echo "the stack exists"
-# 	exit 0
-# fi
+name=$(aws cloudformation wait stack-exists --stack-name $var1 2>&1)
+#echo $name
+#echo $?
 
-
+if [[ -z $name ]];then		
+	echo "the stack exists. please enter a different name"
+	exit 0
+fi
 
 aws cloudformation create-stack --stack-name $var1 --parameters ParameterKey=vpcName,ParameterValue=${var1}-csye6225-vpc ParameterKey=gatewayName,ParameterValue=${var1}-csye6225-InternetGateway ParameterKey=tableName,ParameterValue=${var1}-csye6225-public-route-table --template-body file://csye6225-cf-networking.json
 
 aws cloudformation wait stack-create-complete --stack-name $var1
+
+aws cloudformation describe-stacks --stack-name $var1 --query "Stacks[*].StackStatus" --output text
 
 exit 0
--- infrastructure/aws/cloudformation/csye6225-aws-cf-terminate-stack.sh
+++ infrastructure/aws/cloudformation/csye6225-aws-cf-terminate-stack.sh
@@ -1,14 +1,22 @@
 #!/bin/bash
 
 var1="$1"
-#aws cloudformation wait stack-exists --stack-name $var1
-# echo $?
+#name=$(aws cloudformation describe-stacks --stack-name $var1 --query "Stacks[*].StackName" --output text 2>&1)
+name=$(aws cloudformation wait stack-exists --stack-name $var1 2>&1)
+#echo $?
+#echo $name
 
-# if [ "$?" -eq "255" ];then	
-# 	echo "the stack does not exists"
-# 	exit 1
-# fi
+
+if [[ ! -z $name ]];then		
+	echo "the stack does not exist. please enter a different name"
+	exit 0
+fi
+
+id=$(aws cloudformation describe-stacks --stack-name $var1 --query "Stacks[*].StackId" --output text 2>&1)
 
 aws cloudformation delete-stack --stack-name $var1
 
 aws cloudformation wait stack-delete-complete --stack-name $var1
+aws cloudformation describe-stacks --stack-name $id --query "Stacks[*].StackStatus" --output text
+
+exit 0
