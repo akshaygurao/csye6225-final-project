70ab0e0b461484288bbf08599514e16fa9bf1450
Akshay <gurao.a@husky.neu.edu>
2018-02-03 03:01:45 -0500

    Added functionality and code changes

--- webapp/src/main/java/com/csye6225/spring2018/controller/UserController.java
+++ webapp/src/main/java/com/csye6225/spring2018/controller/UserController.java
@@ -4,18 +4,26 @@ import com.csye6225.spring2018.UserRepository;
 import com.csye6225.spring2018.User;
 
 import com.google.gson.JsonObject;
+import org.apache.catalina.servlet4preview.http.HttpServletRequest;
+import org.apache.http.HttpResponse;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpRequest;
 import org.springframework.http.MediaType;
 import org.springframework.security.crypto.bcrypt.BCrypt;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
 import java.util.HashMap;
 
 
-@RestController
+@Controller
 public class UserController {
     private final static Logger logger = LoggerFactory.getLogger(UserController.class);
 
@@ -23,25 +31,33 @@ public class UserController {
     private UserRepository userRepository;
 
 
-    @PostMapping(value="/loginUser", consumes = "application/json", produces = "application/json")
+    @RequestMapping(value="/loginUser", consumes = "application/json", produces = "application/json", method = RequestMethod.POST)
     @ResponseBody
-    public String login(@RequestBody String js) throws ParseException {
+    public String login(@RequestBody String js, HttpServletRequest request, HttpServletResponse response) throws ParseException, IOException {
+
         JSONParser parser = new JSONParser();
         HashMap<String, String> map = (HashMap<String, String>) parser.parse(js);
         for (User u : userRepository.findAll()) {
             if (u.getEmail().equals(map.get("email")) && BCrypt.checkpw(map.get("password"), u.getPassword())) {
                 logger.info("Loading user home page.");
+                HttpSession session = request.getSession(true);
+                session.setAttribute("email", map.get("email"));
                 System.out.println("Found!");
                 JsonObject json = new JsonObject();
                 json.addProperty("response", "Login Success!");
+                try {
+                    response.sendRedirect("/loginSuccess");
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
                 return json.toString();
             }
         }
         System.out.println("User Not found");
-        return "error";
+        return new JsonObject().toString();
     }
 
-    @PostMapping(value = "/registerUser", consumes = "application/json", produces = "application/json")
+    @RequestMapping(value = "/registerUser", consumes = "application/json", produces = "application/json", method = RequestMethod.POST)
     @ResponseBody
     public String createUser(@RequestBody String js) throws ParseException {
         logger.info("Registering user account with information: {}");
