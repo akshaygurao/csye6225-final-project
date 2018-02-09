9b2aa5b113ec301ffd1da541ace702bf08ce24fc
Akshay <gurao.a@husky.neu.edu>
2018-02-09 16:30:30 -0500

    Added session changes

--- webapp/src/main/java/com/csye6225/spring2018/controller/UserController.java
+++ webapp/src/main/java/com/csye6225/spring2018/controller/UserController.java
@@ -5,11 +5,14 @@ import com.csye6225.spring2018.User;
 
 import com.google.gson.JsonObject;
 import org.apache.catalina.servlet4preview.http.HttpServletRequest;
+import org.apache.http.HttpResponse;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpRequest;
+import org.springframework.http.MediaType;
 import org.springframework.security.crypto.bcrypt.BCrypt;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
@@ -31,22 +34,15 @@ public class UserController {
     @RequestMapping(value="/loginUser", consumes = "application/json", produces = "application/json", method = RequestMethod.POST)
     @ResponseBody
     public String login(@RequestBody String js, HttpServletRequest request, HttpServletResponse response) throws ParseException, IOException {
-
         JSONParser parser = new JSONParser();
         HashMap<String, String> map = (HashMap<String, String>) parser.parse(js);
         for (User u : userRepository.findAll()) {
             if (u.getEmail().equals(map.get("email")) && BCrypt.checkpw(map.get("password"), u.getPassword())) {
-                logger.info("Loading user home page.");
                 HttpSession session = request.getSession(true);
                 session.setAttribute("email", map.get("email"));
                 System.out.println("Found!");
                 JsonObject json = new JsonObject();
                 json.addProperty("response", "Login Success!");
-                try {
-                    response.sendRedirect("/loginSuccess");
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
                 return json.toString();
             }
         }
@@ -56,26 +52,24 @@ public class UserController {
 
     @RequestMapping(value = "/registerUser", consumes = "application/json", produces = "application/json", method = RequestMethod.POST)
     @ResponseBody
-    public String createUser(@RequestBody String js) throws ParseException {
-        logger.info("Registering user account with information: {}");
-        System.out.println(js);
+    public String createUser(@RequestBody String json, HttpServletResponse response) throws ParseException, IOException {
+        logger.info("Registering user account with information");
         JSONParser parser = new JSONParser();
-        HashMap<String, String> map = (HashMap<String, String>) parser.parse(js);
+        HashMap<String, String> map = (HashMap<String, String>) parser.parse(json);
+        JsonObject js = new JsonObject();
         for (User u : userRepository.findAll()){
             if (u.getEmail().equals(map.get("email"))) {
                 logger.info("User already exists.");
-                return "error";
+                js.addProperty("message","User already exists! Please sign in.");
+                return js.toString();
             }
         }
-        System.out.println(map.toString());
-        System.out.println(map);
         User newUser = new User();
         newUser.setEmail(String.valueOf(map.get("email")));
         newUser.setPassword(BCrypt.hashpw(String.valueOf(map.get("password")),BCrypt.gensalt()));
         userRepository.save(newUser);
-        JsonObject json = new JsonObject();
-        json.addProperty("response","Success!");
-        return json.toString();
+        js.addProperty("message", "User added successfully!");
+        return js.toString();
     }
 }
 
