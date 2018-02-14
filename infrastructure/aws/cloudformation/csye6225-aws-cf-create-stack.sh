2f74968dd38bad2f52d2e946c1bbeb0bebfded37
s-rohil <subramanian.roh@northeastern.edu>
2018-02-14 18:47:41 -0500

    updated networking stack with assignment5 requirements

--- infrastructure/aws/cloudformation/csye6225-aws-cf-create-stack.sh
+++ infrastructure/aws/cloudformation/csye6225-aws-cf-create-stack.sh
@@ -8,12 +8,12 @@ name=$(aws cloudformation wait stack-exists --stack-name $var1 2>&1)
 #echo $name
 #echo $?
 
-if [[ -z $name ]];then		
+if [[ -z $name ]];then
 	echo "the stack exists. please enter a different name"
 	exit 0
 fi
 
-aws cloudformation create-stack --stack-name $var1 --parameters ParameterKey=vpcName,ParameterValue=${var1}-csye6225-vpc ParameterKey=gatewayName,ParameterValue=${var1}-csye6225-InternetGateway ParameterKey=tableName,ParameterValue=${var1}-csye6225-public-route-table --template-body file://csye6225-cf-networking.json
+aws cloudformation create-stack --stack-name $var1 --parameters ParameterKey=vpcName,ParameterValue=${var1}-csye6225-vpc ParameterKey=gatewayName,ParameterValue=${var1}-csye6225-InternetGateway ParameterKey=publicroutetableName,ParameterValue=${var1}-csye6225-public-route-table ParameterKey=privateroutetableName,ParameterValue=${var1}-csye6225-private-route-table --template-body file://csye6225-cf-networking.json
 
 aws cloudformation wait stack-create-complete --stack-name $var1
 
