fcb305994508620eb0258809a8b39f85b8dce1a7
Akshay <gurao.a@husky.neu.edu>
2018-02-24 10:20:38 -0500

    Changes to webapp and added dependancies

--- webapp/src/main/java/com/csye6225/spring2018/SpringBootWebApplication.java
+++ webapp/src/main/java/com/csye6225/spring2018/SpringBootWebApplication.java
@@ -8,6 +8,11 @@ import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
 @SpringBootApplication
 public class SpringBootWebApplication {
 
+  @Override
+  protected SpringApplicationBuilder configure(SpringApplicationBuilder application){
+    return application.sources(SpringBootWebApplication.class)
+  }
+
   public static void main(String[] args) throws Exception {
     SpringApplication.run(SpringBootWebApplication.class, args);
 
