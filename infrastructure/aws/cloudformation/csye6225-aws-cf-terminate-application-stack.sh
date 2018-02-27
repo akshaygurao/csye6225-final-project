f0b4c9c6f6ed4960abe6ebe0c6ff19bb1edfebaf
s-rohil <subramanian.roh@northeastern.edu>
2018-02-27 13:33:28 -0500

    added dynamic parameters for RDS userdata

--- infrastructure/aws/cloudformation/csye6225-aws-cf-terminate-application-stack.sh
+++ infrastructure/aws/cloudformation/csye6225-aws-cf-terminate-application-stack.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 var1="$1"
+echo "test"
 #name=$(aws cloudformation describe-stacks --stack-name $var1 --query "Stacks[*].StackName" --output text 2>&1)
 
 #validation code
@@ -12,10 +13,10 @@ var1="$1"
 # fi
 
 id=$(aws cloudformation describe-stacks --stack-name $var1 --query "Stacks[*].StackId" --output text 2>&1)
-echo $id
+echo "$id"
 
 instance_id=$(aws cloudformation describe-stack-resources --stack-name $var1 --logical-resource-id EC2Instance --query "StackResources[0].PhysicalResourceId" --output text)
-echo $instance_id
+echo "$instance_id"
 aws ec2 modify-instance-attribute --instance-id $instance_id --no-disable-api-termination
 
 #code for emptying the s3 bucket
